f2abf28e2da622e6a480811d0712cf44a67eb5bb
Willy Huang <breezekiller789@csie.io>
2020-03-08 14:58:33 +0800

    3/8_HW1

--- hw1/cpp_hw1/main.cpp
+++ hw1/cpp_hw1/main.cpp
@@ -10,12 +10,10 @@ int main(int argc, char **argv){
     char *line, *ptr;
     int len;
     struct lnode *head = NewNode();
-    struct lnode *node = NULL;
     line = (char *)malloc(sizeof(char) * MaxLine);
     while(cin.getline(line, MaxLine)){
         len = strlen(line);
         ptr = line;
-        node = head;
         for(int i=0; i<len; i++, ptr++){
             Insert(head, *ptr);
         }
