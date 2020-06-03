ada3aef4bd7b410cd5f26d12fd3749b76ce9d0fc
Willy Huang <breezekiller789@csie.io>
2020-06-03 23:05:03 +0800

    6/3

--- hw6/func.cpp
+++ hw6/func.cpp
@@ -2,6 +2,66 @@
 
 using namespace std;
 
+English::English(int *Grades, char *fn, char *ln, char *subject){
+    for(int i=0; i<4; i++){
+        this->English_Grade[i] = Grades[i];
+        /* printf("%d ", this->English_Grade[i]); */
+    }
+    strcpy(this->course, subject);
+    strcpy(this->first_name, fn);
+    strcpy(this->last_name, ln);
+}
+
+void English::info(){
+    printf("fn = %s\n", this->first_name);
+    printf("ln = %s\n", this->last_name);
+    printf("course = %s\n", this->course);
+    for(int i=0; i<4; i++){
+        printf("%d ", this->English_Grade[i]);
+    }
+    printf("\n");
+}
+
+Math::Math(int *Grades, char *fn, char *ln, char *subject){
+    for(int i=0; i<8; i++){
+        this->Math_Grade[i] = Grades[i];
+        /* printf("%d ", this->Math_Grade[i]); */
+    }
+    strcpy(this->course, subject);
+    strcpy(this->first_name, fn);
+    strcpy(this->last_name, ln);
+}
+
+void Math::info(){
+    printf("fn = %s\n", this->first_name);
+    printf("ln = %s\n", this->last_name);
+    printf("course = %s\n", this->course);
+    for(int i=0; i<8; i++){
+        printf("%d ", this->Math_Grade[i]);
+    }
+    printf("\n");
+}
+
+History::History(int *Grades, char *fn, char *ln, char *subject){
+    for(int i=0; i<3; i++){
+        this->History_Grade[i] = Grades[i];
+        /* printf("%d ", this->History_Grade[i]); */
+    }
+    strcpy(this->course, subject);
+    strcpy(this->first_name, fn);
+    strcpy(this->last_name, ln);
+}
+
+void History::info(){
+    printf("fn = %s\n", this->first_name);
+    printf("ln = %s\n", this->last_name);
+    printf("course = %s\n", this->course);
+    for(int i=0; i<3; i++){
+        printf("%d ", this->History_Grade[i]);
+    }
+    printf("\n");
+}
+
 int rmnewline(char *line){
     char *ptr = line;
     int len=0;
@@ -13,46 +73,129 @@ int rmnewline(char *line){
     return len;
 }
 
-int Parser(char **words, FILE *fp){
-    int i=0;
-    char *ptr, *qtr;
-    char *line = (char *)malloc(sizeof(char) * MaxLine);
+void Get_Subject(const char *ptr, char *subject){
+    char *qtr = subject;
+    while(*ptr && !isspace(*ptr)){
+        *qtr = *ptr;
+        ptr++;
+        qtr++;
+    }
+    *qtr = 0;
+}
 
-    while(fgets(line, MaxLine, fp) != NULL){
+//  Get First Name
+int Get_First_Names(const char *ptr, char *first_name){
+    int cnt=0;
+    char *qtr = first_name;
+    while(*ptr && *ptr != ','){
+        *qtr = *ptr;
+        ptr++;
+        qtr++;
+        cnt++;
+    }
+    *qtr = 0;
+
+    //  skip all the spaces and coma
+    while(*ptr == ',' || isspace(*ptr)){
+        ptr++;
+        cnt++;
+    }
+    return cnt;
+}
+
+//  Get Last Name
+void Get_Last_Names(const char *ptr, char *last_name){
+    char *qtr = last_name;
+    while(*ptr){
+        *qtr = *ptr;
+        ptr++;
+        qtr++;
+    }
+    *qtr = 0;
+}
+
+//  TODO : 最後一行會不見，解決他。
+void Parser(int Num_Students, FILE *fp, Student **list){
+    char *line = (char *)malloc(sizeof(char) * MaxLine);
+    char *ptr, *qtr;
+    char first_name[20];
+    char last_name[20];
+    char subject[10];
+    int English_Grade[4], History_Grade[3], Math_Grade[8];
+    int cnt=0;
+    while(fgets(line, MaxLine, fp)){
         rmnewline(line);
         ptr = line;
-        while(1){
-            while(isspace(*ptr)){
-                ptr++;
-            }
-            if(*ptr == '\0')
-                break;
-            *(words+i) = (char *)malloc(sizeof(char) * strlen(line));
-            qtr = *(words+i);
-            while(*ptr && !isspace(*ptr)){
-                *qtr = *ptr;
-                /* printf("qtr = %c\tptr = %c\n", *qtr, *ptr); */
-                ptr++;
-                qtr++;
-            }
-            /* printf("%s\n", *(words+i)); */
-            i++;
-            if(isspace(*ptr)){
-                while(isspace(*ptr)){
+        /* printf("%s\n", line); */
+        //  如果一開始就空掉，這行就不要做，不然會有bug。
+        if(*ptr == 0)
+            continue;
+        //  拿成績。
+        if(strstr(ptr, "Math") || strstr(ptr, "English") || strstr(ptr, "History")){
+            Get_Subject(ptr, subject);
+            printf("subject = %s\n", subject);
+            //  該項是數學成績。
+            if(strstr(ptr, "Math")){
+                while(!isspace(*ptr)){
                     ptr++;
                 }
-                if(*ptr == '\0')
-                    break;
-                else
-                    continue;
+                ptr++;
+
+                sscanf(ptr, "%d %d %d %d %d %d %d %d", &Math_Grade[0], &Math_Grade[1],
+                        &Math_Grade[2], &Math_Grade[3], &Math_Grade[4], &Math_Grade[5],
+                        &Math_Grade[6], &Math_Grade[7]);
+                /* for(int i=0; i<8; i++){ */
+                /*     printf("%d ", Math_Grade[i]); */
+                /* } */
+                *(list+cnt) = new Math(Math_Grade, first_name, last_name, subject);
+                cnt++;
             }
-            else
-                break;
+            //  該項是英文成績。
+            else if(strstr(ptr, "English")){
+                while(!isspace(*ptr)){
+                    ptr++;
+                }
+                ptr++;
+
+                sscanf(ptr, "%d %d %d %d", &English_Grade[0], &English_Grade[1],
+                        &English_Grade[2], &English_Grade[3]);
+                /* for(int i=0; i<4; i++){ */
+                /*     printf("%d ", English_Grade[i]); */
+                /* } */
+                *(list+cnt) = new English(English_Grade, first_name, last_name, subject);
+                cnt++;
+            }
+            //  該項是歷史成績。
+            else if(strstr(ptr, "History")){
+                while(!isspace(*ptr)){
+                    ptr++;
+                }
+                ptr++;
+
+                sscanf(ptr, "%d %d %d", &History_Grade[0], &History_Grade[1],
+                        &History_Grade[2]);
+                /* for(int i=0; i<3; i++){ */
+                /*     printf("%d ", History_Grade[i]); */
+                /* } */
+                *(list+cnt) = new History(History_Grade, first_name, last_name, subject);
+                cnt++;
+            }
+            printf("\n==========================\n");
+            continue;
         }
+
+        //  Get First Names
+        int shift_amount = Get_First_Names(ptr, first_name);
+        printf("first name = %s\n", first_name);
+
+        ptr += shift_amount;
+
+        //  Get Last Names
+        Get_Last_Names(ptr, last_name);
+        printf("last name = %s\n", last_name);
+
     }
-
-
-    return i;
+    printf("cnt = %d\n", cnt);
 }
 
 
--- hw6/header.h
+++ hw6/header.h
@@ -10,24 +10,43 @@
 #define MaxLine 1024
 using namespace std;
 
-int Parser(char **, FILE *);
+
+int Get_First_Names(const char *, char *);
+void Get_Last_Names(const char *, char *);
+void Get_Subject(const char *, char *);
 
 int rmnewline(char *);
 
 class Student{
     protected:
-        char *first_name;
-        char *last_name;
+        char first_name[20];
+        char last_name[20];
+        char course[10];
+        int English_Grade[4];
+        int History_Grade[3];
+        int Math_Grade[8];
+
+    public:
+        virtual void info() = 0;
 };
 
 class English:public Student{
+    public:
+        English(int *Grades, char *fn, char *ln, char *subject);
+        void info();
 };
 
 class History:public Student{
-
+    public:
+        History(int *Grades, char *fn, char *ln, char *subject);
+        void info();
 };
 
 class Math:public Student{
+    public:
+        Math(int *Grades, char *fn, char *ln, char *subject);
+        void info();
 };
 
 
+void Parser(int, FILE *, Student **);
